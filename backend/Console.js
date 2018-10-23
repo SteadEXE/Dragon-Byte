da6ffeb56981eed5f1c0de8a13c9674750a03a5a
SteadEXE <j0rd4n68@gmail.com>
2018-10-23 16:52:52 +0200

    Increase readability.

--- backend/Console.js
+++ backend/Console.js
@@ -1,19 +1,19 @@
 const chalk = require('chalk')
 
 class Console {
-    info (text, title = 'INFO') {
+    info (text, title = '  INFO  ') {
         console.log(`${chalk.bgBlue.white(title)} ${chalk.white(text)}`)
     }
 
-    positive (text, title = 'SUCCESS') {
+    positive (text, title = '  SUCCESS  ') {
         console.log(`${chalk.bgGreen.white(title)} ${chalk.white(text)}`)
     }
 
-    network (text, title = 'NETWORK') {
-        console.log(`${chalk.bgYellow.white(title)} ${chalk.white(text)}`)
+    network (text, title = '  NETWORK  ') {
+        console.log(`${chalk.bgYellow.white(title)} ${chalk.black(text)}`)
     }
 
-    error (text, title = 'ERROR') {
+    error (text, title = '  ERROR  ') {
         console.log(`${chalk.bgRed.white(title)} ${chalk.white(text)}`)
     }
 }
