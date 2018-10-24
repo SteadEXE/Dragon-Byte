ce3ba294b3710f41a417818769436a47057f48d1
SteadEXE <j0rd4n68@gmail.com>
2018-10-24 13:44:24 +0200

    Fix responsive on login page.

---
+++ backend/Listeners/UserListener.js
@@ -0,0 +1,9 @@
+const { EventEmitter } = require('events')
+
+class UserListener {
+    constructor () {
+
+    }
+}
+
+module.exports = new UserListener ()
--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,15 +1,15 @@
 <template>
     <div>
         <div class="row pt-4" v-if="authentified">
-            <div class="col-4 offset-4">
+            <div class="col-sm-4 offset-sm-4">
                 <login></login>
             </div>
         </div>
         <div class="row pt-4" v-else>
-            <div class="col-3 offset-3">
+            <div class="col-sm-3 offset-sm-3 mb-4">
                 <login></login>
             </div>
-            <div class="col-3">
+            <div class="col-sm-3">
                 <register></register>
             </div>
         </div>
