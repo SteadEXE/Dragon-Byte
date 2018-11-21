2a9d9a50b97219a0944790463428810db19a7796
SteadEXE <j0rd4n68@gmail.com>
2018-11-21 16:01:52 +0100

    Tweak users list.

--- frontend/src/components/users/User.vue
+++ frontend/src/components/users/User.vue
@@ -3,14 +3,19 @@
         <div class="card-header py-2 font-weight-bold text-light">
             <i class="fas fa-user-circle mr-2"></i> {{ user.nickname }}
             <span class="text-primary">
-                <i class="fas fa-star mx-2"></i> {{ user.experience }}
+                <i class="fas fa-star mx-2"></i> <animated-number :n="user.experience"></animated-number>
             </span>
         </div>
     </div>
 </template>
 
 <script>
+    import AnimatedNumber from '@/components/AnimatedNumber'
+
     export default {
+        components: {
+            animatedNumber: AnimatedNumber
+        },
         props: {
             user: Object
         }
